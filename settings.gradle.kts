627269744bcbd5a7c111f1f157eeaa9cd913ef6d
Csiszer <csiszerors@gmail.com>
2024-03-23 15:35:44 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "oopLab5"
+
---
+++ src/main/java/org/postuniv/serviceAuto/Main.java
@@ -0,0 +1,7 @@
+package org.postuniv.serviceAuto;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
