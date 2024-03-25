48e7fa338510c1b4c032b1627e82a385fb1148ea
Örs Csiszer <csiszerors@gmail.com>
2024-03-25 19:11:12 +0200

    clientCard class is ready

--- src/main/java/org/postuniv/serviceAuto/domain/ClinetCard.java
+++ src/main/java/org/postuniv/serviceAuto/domain/ClinetCard.java
@@ -1,4 +1,15 @@
 package org.postuniv.serviceAuto.domain;
 
+import java.util.Date;
+
 public class ClinetCard {
+    //the ID is unique
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String CNP;
+    private Date birthDate;
+    private Date registrationDate;
+
+
 }
