18c7a28725ad140c483897a3574c4718e2825e6e
Örs Csiszer <csiszerors@gmail.com>
2024-03-29 16:02:43 +0200

    MeniuOptions class is ready to use

--- src/main/java/org/postuniv/serviceAuto/ui/Console.java
+++ src/main/java/org/postuniv/serviceAuto/ui/Console.java
@@ -14,4 +14,17 @@ public class Console {
         System.out.println("Main menu implementation");
     }
 
+
+
+
+
+
+
+//    3.4. Căutare mașini și clienți. Căutare full text.
+//3.5. Afișarea tuturor tranzacțiilor cu suma cuprinsă într-un interval dat.
+//3.6. Afișarea mașinilor ordonate descrescător după suma obținută pe manoperă.
+//3.7. Afișarea cardurilor client ordonate descrescător după valoarea reducerilor obținute.
+//3.8. Ștergerea tuturor tranzacțiilor dintr-un anumit interval de zile.
+//3.9. Actualizarea garanției la fiecare mașină: o mașină este în garanție dacă și numai dacă are maxim 3 ani de la achiziție și maxim 60 000 de km.
+
 }
