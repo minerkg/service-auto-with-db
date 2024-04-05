a8bfbba01528ead5580803b4ed90a771041336ef
Csiszer <csiszerors@gmail.com>
2024-04-05 21:01:06 +0300

    generic repo impl ready

--- src/main/java/org/postuniv/serviceAuto/repository/CarServiceRepoImpl.java
+++ src/main/java/org/postuniv/serviceAuto/repository/CarServiceRepoImpl.java
@@ -1,9 +1,11 @@
 package org.postuniv.serviceAuto.repository;
 
+import org.postuniv.serviceAuto.domain.Entity;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarServiceRepoImpl <T> implements CarServiceRepo <T> {
+public class CarServiceRepoImpl <T extends Entity> implements CarServiceRepo <T> {
 
     private List<T> entityList;
 
@@ -23,16 +25,23 @@ public class CarServiceRepoImpl <T> implements CarServiceRepo <T> {
 
     @Override
     public boolean update(T entity) {
-        return entityList.id;
+        entityList = entityList.stream().
+                map(ent -> ent.getId() == entity.getId() ? ent = entity : ent).
+                toList();
+        return true; /// TODO: isn't solved yet
     }
 
     @Override
     public T findById(int id) {
+        for (T entry: entityList) {
+            if (entry.getId() == id) return entry;
+        }
         return null;
     }
 
     @Override
     public boolean delete(int entityId) {
-        return false;
+        return this.entityList.
+                removeIf(entr -> entr.getId() == entityId);
     }
 }
